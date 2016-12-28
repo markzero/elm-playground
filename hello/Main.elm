ad2d4dfb677c69b68e575d7c0f38019c4f67bc2d
Marko Jakic <jakicmeister@gmail.com>
2016-12-28 23:07:09 +0100

    bunch of new concepts learned in one line of code

--- hello/Main.elm
+++ hello/Main.elm
@@ -12,6 +12,14 @@ ask thing place =
         ++ thing ++ " in the " ++ place ++ "?"
 
 askPolitelyAboutFish : String -> String
+
+-- key concepts to observe here:
+-- 1. function composition (<<) - make a function out of two+ functions
+-- 2. (ask "fish") => partially applied function - only one arg instead of two
+-- 3. point free style: not defining arguments directly
+-- (ask "fish") is curried function, if called with less params
+-- it returns a function which accepts string and returns a string
+-- `place` argument is passed to `politely` fn through `ask` fn
 askPolitelyAboutFish = politely << (ask "fish")
 
 main =
