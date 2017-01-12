2d78f07f66ffad65cc867a48cf6d729f24f5eee3
Marko Jakic <jakicmeister@gmail.com>
2017-01-12 22:34:07 +0100

    add boilerplate using beginnerProgram

---
+++ beginnerProgram-boilerplate.elm
@@ -0,0 +1,46 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- model
+type alias Model =
+    { str: String
+    , list: List Int
+    }
+
+model =
+    { str = ""
+    , list = []
+    }
+
+-- update
+type Msg
+    = SomeUpdate String -- string is parameter
+    | OtherUpdate -- no params
+
+update: Msg -> Model -> Model
+update msg model =
+    case msg of
+        SomeUpdate strarg ->
+            { model | str = strarg }
+        OtherUpdate ->
+            { model | list = 1 :: model.list }
+
+-- view
+view model =
+    div []
+    [ input [onInput SomeUpdate] [text "Do the string thing"]
+    , button [onClick OtherUpdate] [text "Do the list thing"]
+    , p [] [text (toString model.list)]
+    , p [] [text model.str]
+    ]
+
+main =
+    Html.beginnerProgram
+    { model  = model
+    , view   = view
+    , update = update
+    }
